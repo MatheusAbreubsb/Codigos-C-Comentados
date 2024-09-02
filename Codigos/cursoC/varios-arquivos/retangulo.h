69d3ff44a84283ba6ca1942aa40c181bcae5ebf8
Matheus_Abreu <matheusabreubsb@gmail.com>
2024-09-02 20:38:21 -0300

    Revert "Commitando Todos os codigos C"
    This reverts commit 3adfa8c67effd1bbd882addda0a4e16a79b70178.

--- Codigos/cursoC/varios-arquivos/retangulo.h
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-// aqui foi prototipada a função ela será incluida nos arquivos tipo C para ser chamada e/ou trabalhada
-float calcAreaRet(float lado1, float lado2);
